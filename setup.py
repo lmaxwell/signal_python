c698df5621b00c98b925262f20a37fc928738179
lixian <shysian@gmail.com>
2019-12-27 17:41:19 +0800

    package

---
+++ setup.py
@@ -0,0 +1,13 @@
+    
+from setuptools import setup,find_packages
+
+
+packages=find_packages(where=".")
+
+setup(
+    name='signal_python',
+    version='0.1',
+    description='',
+    author='',
+    packages=packages,
+)
